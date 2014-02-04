455b92ad6745e36b23d1988af23f24c90e96930c
John <j.martin0027@gmail.com>
2014-02-04 03:33:15 +0000

    changed the form template and the layout for adding callbacks

--- app/page/forms.py
+++ app/page/forms.py
@@ -1,18 +1,18 @@
 from flask.ext.wtf import Form
-from wtforms import TextAreaField, TextField, BooleanField, validators, ValidationError
+from wtforms import TextAreaField, TextField, BooleanField, validators, ValidationError, IntegerField
 from wtforms.validators import Required, NumberRange, Regexp
 
 
 class CallbackSubmit(Form):
     #rec = TextField('TestRecipient', [validators.Required('Please enter the testing recipient')])
-    ddi = TextField('DDI',
+    ddi = IntegerField('DDI',
                     [Required('Please enter the DDI'),
                     Regexp(regex='\d{4,10}',
                            message="Must enter at least 4 digits")])
-    ticket = TextField('Ticket',
+    ticket = IntegerField('Ticket',
                        [Regexp(regex='\d{4,10}',
                                message='Must enter at least 4 digits')])
-    phone = TextField('Phone',
+    phone = IntegerField('Phone',
                       [Required('Please enter the phone number'),
                       Regexp(regex='\d{10,15}',
                                   message='Must enter 10 digits')])
